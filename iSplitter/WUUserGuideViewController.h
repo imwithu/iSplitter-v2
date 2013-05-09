b7f979aad3562303cff8083d76d0eeb39b5f7ade
James Yu <james@withu.im>
2013-05-09 01:00:01 -0700

    增加了用户引导。2.0.0 release

---
+++ iSplitter/WUUserGuideViewController.h
@@ -0,0 +1,13 @@
+//
+//  WUUserGuideViewController.h
+//  iSplitter
+//
+//  Created by James Yu on 5/8/13.
+//  Copyright (c) 2013 Jianqiang Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WUUserGuideViewController : UIViewController
+
+@end
---
+++ iSplitter/WUUserGuideViewController.m
@@ -0,0 +1,38 @@
+//
+//  WUUserGuideViewController.m
+//  iSplitter
+//
+//  Created by James Yu on 5/8/13.
+//  Copyright (c) 2013 Jianqiang Yu. All rights reserved.
+//
+
+#import "WUUserGuideViewController.h"
+
+@interface WUUserGuideViewController ()
+
+@end
+
+@implementation WUUserGuideViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
